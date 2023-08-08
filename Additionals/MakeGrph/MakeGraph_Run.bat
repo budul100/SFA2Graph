26c75ca32fcbd6e819181877b873b58762cf177f
budul100 <MarekGrochowski@web.de>
2023-08-08 15:08:48 -0400

    Graph creation scripts improved

--- Additionals/MakeGrph/MakeGraph_Run.bat
+++ Additionals/MakeGrph/MakeGraph_Run.bat
@@ -1,5 +1,27 @@
+@echo off
+
+echo.
+echo *** Remove existing folder ***
+echo.
+
 rmdir /s /q .\RoutingGraph
+
+echo.
+echo *** Create graph ***
+echo.
 
 C:\IVU.plan\bin64\ICR_MakeGraph.exe -v .\MakeGraph.param
 
+echo.
+echo *** Copy additional files ***
+echo.
+
 xcopy /e /v .\Template .\RoutingGraph
+
+echo.
+echo *** Create zip file ***
+echo.
+
+pushd .\RoutingGraph
+tar -a -c -f ..\RoutingGraph.zip *
+popd
